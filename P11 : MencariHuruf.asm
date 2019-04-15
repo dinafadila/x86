771a6d384f3f0cd98444b742d2ad10db8b9d4102
dina <39585270+dinafadila@users.noreply.github.com>
2019-04-15 17:23:42 +0700

    Update P11 : MencariHuruf.asm

--- P11 : MencariHuruf.asm
+++ P11 : MencariHuruf.asm
@@ -4,38 +4,39 @@ extern scanf
      
 section .data
     input db "%s %s", 0
-    fmtprint db "%d", 10, 0
+    print db "%d", 10, 0
     no db "tidak ada", 10, 0
     
          
 section .bss
 	string1 resb 51		;panjang string ditambah nul
-	cari resb 2			;panjang string yg dicari ditambah nul
+	cari resb 2		;panjang string yg dicari ditambah nul
         
 section .text
     global main
          
 main:
-	cld					;biar ngebaca string dari depan
+	cld			;biar ngebaca string dari depan
 	push cari		
 	push string1
 	push input
 	call scanf
 	
-	mov ecx, 50			;buat menyimpan counter
-	mov edi, str1		;
-	mov al, [cari]		;
-	repne scasb			;
+	mov ecx, 50		;buat menyimpan counter
+	mov edi, string1	;buat nyimpen string (esi,edi, dan al)
+	mov eax, [cari]		;nah kalo scasb dia cuman bisa make angka atau char(al,ax,eax) dan string (edi)
+	repne scasb		;repne : buat looping sampe ketemu yg sama dengan yg dicari kalo ga ada looping terus
+				;scasb : buat bandingin satu satu
 	
 	
-	mov eax, 50
-	sub eax, ecx
-	mov ebx, 0
-	cmp ecx, ebx
-	je langsung
+	mov eax, 50		;ini buat simpen total awal
+	sub eax, ecx		;letaknya = 50-45 = 5
+	mov ebx, 0		;ebx buat nyimpen angka 0
+	cmp ecx, ebx		;dibandingin kalo ecx=0
+	je langsung		;dia lompat ke langsung
 	
 	push eax
-	push fmtprint
+	push print
 	call printf
 	jmp exit
 
@@ -47,6 +48,6 @@ langsung:
 exit:
 	push 0
 	call fflush
-    mov eax, 1
-    mov ebx, 0
-    int 0x80
+    	mov eax, 1
+    	mov ebx, 0
+    	int 0x80
